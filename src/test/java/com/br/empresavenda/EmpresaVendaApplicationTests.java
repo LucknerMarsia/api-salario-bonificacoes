8b756e475b356a7c692e10db0ba1b06785bc19f8
LucknerMarsia <lucknermarsia@gmail.com>
2024-05-18 20:51:13 -0300

    Versão inicial do projeto

---
+++ src/test/java/com/br/empresavenda/EmpresaVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.empresavenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
